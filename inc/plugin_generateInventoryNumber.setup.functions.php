9d4d2869a94dbb9528635f94681975aff8ac2be0
walid <walid@349b9182-4a13-0410-896f-e5e9767dd1b3>
2008-09-04 14:24:54 +0000

    [generateInventoryNumber] Correction boulette
    git-svn-id: https://forge.indepnet.net/svn/geninventorynumber/trunk@5 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_generateInventoryNumber.setup.functions.php
+++ inc/plugin_generateInventoryNumber.setup.functions.php
@@ -57,7 +57,7 @@ function plugin_generateInventoryNumber_Install() {
 	) ENGINE=MyISAM  DEFAULT CHARSET=latin1;";
 	$DB->query($sql) or die($DB->error());
 	
-	$sql = "INSERT INTO `glpi071`.`glpi_plugin_generateinventorynumber_config` (
+	$sql = "INSERT INTO `glpi_plugin_generateinventorynumber_config` (
 			`ID` ,`FK_entities` ,`active` ,`template_computer` ,`template_monitor` ,`template_printer` ,`template_peripheral` ,`template_phone` ,`template_software` ,`template_networking`,`next_number`)
 			VALUES (NULL , '-1', '0', '#######', '#######', '#######', '#######', '#######', '#######', '#######','0');";
 	$DB->query($sql) or die($DB->error());
