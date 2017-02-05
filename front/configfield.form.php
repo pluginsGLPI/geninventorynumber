b4b6013fbae3abf143cb49fb3a82153ce4ef3311
Walid <wnouh@teclib.com>
2017-02-05 22:28:37 +0100

    Update headers

--- front/configfield.form.php
+++ front/configfield.form.php
@@ -1,30 +1,36 @@
 <?php
 /*
- * @version $Id: configfield.form.php 79 2013-01-21 08:56:14Z walid $
+ * @version $Id: HEADER 15930 2011-10-25 10:47:55Z orthagh $
  -------------------------------------------------------------------------
- geninventorynumber - plugin for GLPI
- Copyright (C) 2003-2011 by the geninventorynumber Development Team.
- This file is part of the geninventorynumber plugin.
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
 
- geninventorynumber plugin is free software; you can redistribute it and/or modify
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- geninventorynumber plugin is distributed in the hope that it will be useful,
+ GLPI is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- This file is part of geninventorynumber plugin.
+
  You should have received a copy of the GNU General Public License
- along with GLPI; along with geninventorynumber. If not, see <http://www.gnu.org/licenses/>.
+ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  @package   geninventorynumber
  @author    the geninventorynumber plugin team
- @copyright Copyright (c) 2008-2013 geninventorynumber plugin team
+ @copyright Copyright (c) 2008-2017 geninventorynumber plugin team
  @license   GPLv2+
             http://www.gnu.org/licenses/gpl.txt
- @link      https://forge.indepnet.net/projects/geninventorynumber
+ @link      https://github.com/pluginsGLPI/geninventorynumber
  @link      http://www.glpi-project.org/
  @since     2008
  ---------------------------------------------------------------------- */
--- inc/profile.class.php
+++ inc/profile.class.php
@@ -1,29 +1,36 @@
 <?php
 /*
- * @version $Id: bill.tabs.php 530 2011-06-30 11:30:17Z walid $
+ * @version $Id: HEADER 15930 2011-10-25 10:47:55Z orthagh $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
  LICENSE
 
- This file is part of the geninventorynumber plugin.
+ This file is part of GLPI.
 
- geninventorynumber plugin is free software; you can redistribute it and/or modify
+ GLPI is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- geninventorynumber plugin is distributed in the hope that it will be useful,
+ GLPI is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with GLPI; along with geninventorynumber. If not, see <http://www.gnu.org/licenses/>.
+ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  @package   geninventorynumber
  @author    the geninventorynumber plugin team
- @copyright Copyright (c) 2008-2013 geninventorynumber plugin team
+ @copyright Copyright (c) 2008-2017 geninventorynumber plugin team
  @license   GPLv2+
             http://www.gnu.org/licenses/gpl.txt
- @link      https://forge.indepnet.net/projects/geninventorynumber
+ @link      https://github.com/pluginsGLPI/geninventorynumber
  @link      http://www.glpi-project.org/
  @since     2008
  ---------------------------------------------------------------------- */
