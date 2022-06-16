7b08a005aa485d33af20c1f654bcb21f43d1b5ec
Curtis Conard <cconard96@gmail.com>
2022-06-16 08:26:48 -0400

    Auto-reset index feature

--- inc/config.class.php
+++ inc/config.class.php
@@ -39,6 +39,27 @@ class PluginGeninventorynumberConfig extends CommonDBTM {
    static $rightname = 'config';
    public $dohistory = true;
 
+   /**
+    * Never reset the index
+    * @var int
+    */
+   public const AUTO_RESET_NONE = 0;
+   /**
+    * Reset the index if the last number was generated on a different day
+    * @var int
+    */
+   public const AUTO_RESET_DAILY = 1;
+   /**
+    * Reset the index if the last number was generated on a different month
+    * @var int
+    */
+   public const AUTO_RESET_MONTHLY = 2;
+   /**
+    * Reset the index if the last number was generated on a different year
+    * @var int
+    */
+   public const AUTO_RESET_YEARLY = 3;
+
    static function getTypeName($nb = 0) {
       return __('Inventory number generation', 'geninventorynumber');
    }
@@ -107,6 +128,19 @@ class PluginGeninventorynumberConfig extends CommonDBTM {
       return $sopt;
    }
 
+    /**
+     * Get all auto-reset options
+     * @return array
+     */
+   public static function getAutoResetOptions(): array {
+       return [
+           self::AUTO_RESET_NONE => __('Never', 'geninventorynumber'),
+           self::AUTO_RESET_DAILY => __('Daily', 'geninventorynumber'),
+           self::AUTO_RESET_MONTHLY => __('Monthly', 'geninventorynumber'),
+           self::AUTO_RESET_YEARLY => __('Yearly', 'geninventorynumber'),
+       ];
+   }
+
    function showForm($id, $options = []) {
       if ($id > 0) {
           $this->getFromDB($id);
@@ -118,14 +152,62 @@ class PluginGeninventorynumberConfig extends CommonDBTM {
 
       $twig = TemplateRenderer::getInstance();
       $twig->display('@geninventorynumber/config.html.twig', [
-         'item'   => $this,
-         'params' => $options,
+         'item'               => $this,
+         'params'             => $options,
+         'auto_reset_methods' => self::getAutoResetOptions(),
       ]);
       return true;
    }
 
+   /**
+    * Check if the index needs to be reset based on the configured auto-reset method
+    * @return bool
+    */
+   static function needIndexReset(): bool {
+       $config = new self();
+       $config->getFromDB(1);
+
+       if (
+           $config->fields['auto_reset_method'] === self::AUTO_RESET_NONE
+           || $config->fields['date_last_generated'] === null
+       ) {
+           return false;
+       }
+
+       $current_date = strtotime($_SESSION['glpi_currenttime']);
+       $last_gen_date = strtotime($config->fields['date_last_generated']);
+
+       switch ($config->fields['auto_reset_method']) {
+           case self::AUTO_RESET_DAILY:
+               return date('Y-m-d', $last_gen_date) !== date('Y-m-d', $current_date);
+           case self::AUTO_RESET_MONTHLY:
+               return date('Y-m', $last_gen_date) !== date('Y-m', $current_date);
+           case self::AUTO_RESET_YEARLY:
+               return date('Y', $last_gen_date) !== date('Y', $current_date);
+       }
+       return false;
+   }
+
+   /**
+    * Reset the index to 0 and reset the last generated date
+    */
+   public static function resetIndex(): void {
+       global $DB;
+
+       $DB->update(self::getTable(), [
+           'index' => 0,
+           'date_last_generated' => $_SESSION['glpi_currenttime']
+       ], [
+           'id' => 1
+       ]);
+   }
+
    static function getNextIndex() {
       global $DB;
+
+      if (self::needIndexReset()) {
+          self::resetIndex();
+      }
 
       $query = "SELECT `index`
                 FROM `".getTableForItemType(__CLASS__)."`";
@@ -171,6 +253,8 @@ class PluginGeninventorynumberConfig extends CommonDBTM {
              `is_active` tinyint  NOT NULL default 0,
              `index` int  NOT NULL default 0,
              `comment` text,
+             `date_last_generated` timestamp NULL DEFAULT NULL,
+             `auto_reset_method` int unsigned NOT NULL default '0',
              PRIMARY KEY  (`id`)
              ) ENGINE=InnoDB DEFAULT CHARSET={$default_charset} COLLATE={$default_collation} ROW_FORMAT=DYNAMIC;";
          $DB->query($sql) or die($DB->error());
@@ -195,6 +279,8 @@ class PluginGeninventorynumberConfig extends CommonDBTM {
 
          $migration->dropField($table, 'FK_entities');
          $migration->dropField($table, 'entities_id');
+         $migration->addField($table, 'date_last_generated', 'timestamp');
+         $migration->addField($table, 'auto_reset_method', "int unsigned NOT NULL default '0'");
       }
 
       //Remove unused table
@@ -212,7 +298,8 @@ class PluginGeninventorynumberConfig extends CommonDBTM {
       global $DB;
 
       $query = "UPDATE `".getTableForItemType(__CLASS__)."`
-                SET `index`=`index`+1";
+                SET `index`=`index`+1,`date_last_generated`='{$_SESSION['glpi_currenttime']}'
+                WHERE `is_active`='1'";
       $DB->query($query);
    }
 
