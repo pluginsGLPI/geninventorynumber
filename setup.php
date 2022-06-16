58acc8b147454f35f40e25cf6c3791d3cf74e8d0
Cédric Anne <cedric.anne@gmail.com>
2022-06-16 14:46:26 +0200

    2.7.0

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_GENINVENTORYNUMBER_VERSION', '2.6.0');
+define ('PLUGIN_GENINVENTORYNUMBER_VERSION', '2.7.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENINVENTORYNUMBER_MIN_GLPI", "10.0.0");
