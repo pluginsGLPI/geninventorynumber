5e93f439de31327ceb473036cf8fe955c41a33e5
AdrienClairembault <aclairembault@teclib.com>
2022-10-10 13:47:05 +0200

    Fix \s and \n placeholders for datainjection

--- inc/generation.class.php
+++ inc/generation.class.php
@@ -95,8 +95,8 @@ class PluginGeninventorynumberGeneration {
             date('Y'),
             date('m'),
             date('d'),
-            $item->fields['serial'] ?? '',
-            $item->fields['name'] ?? '',
+            $item->input['serial'] ?? '',
+            $item->input['name'] ?? '',
             '',
          ],
          $autonum
