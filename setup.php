a6285eccffba5287ef1bee7cddeb26dc9f650114
Romain B <8530352+Rom1-B@users.noreply.github.com>
2024-06-12 07:57:05 +0200

    2.8.5 (#94)

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_GENINVENTORYNUMBER_VERSION', '2.8.4');
+define('PLUGIN_GENINVENTORYNUMBER_VERSION', '2.8.5');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENINVENTORYNUMBER_MIN_GLPI", "10.0.11");
