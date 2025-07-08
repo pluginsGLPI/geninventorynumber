9c58b169a63929623ccd40fbc9d3aa7a6e822a16
Stanislas <skita@teclib.com>
2025-07-08 11:50:54 +0200

    CS fix

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -180,7 +180,8 @@ class PluginGeninventorynumberProfile extends CommonDBTM
             $item instanceof CommonDBTM
             && $item->fields['interface'] == 'central'
         ) {
-            return self::createTabEntry(PluginGeninventorynumberConfig::getTypeName(0), 0, $item::getType(), PluginGeninventorynumberConfig::getIcon());   ;
+            return self::createTabEntry(PluginGeninventorynumberConfig::getTypeName(0), 0, $item::getType(), PluginGeninventorynumberConfig::getIcon());
+            ;
         }
 
         return '';
